6c6fab1979b1650e4578652296ad7205a31595c0
CLIENTHOSTING <149566586+CLIENTHOSTING@users.noreply.github.com>
2024-01-14 22:01:15 +0300

    main

---
+++ main.js
@@ -0,0 +1,6 @@
+const input = document.querySelector('.main-input')
+const searchBtn = document.querySelector('.search-button')
+
+searchBtn.addEventListener('click',(event) => {
+    console.log(input.value)
+})
