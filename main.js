c3fba32009f2870deed168dabe76cc9a3d44bf41
CLIENTHOSTING <149566586+CLIENTHOSTING@users.noreply.github.com>
2024-01-16 09:14:23 +0300

    main

--- main.js
+++ main.js
@@ -1,6 +1,22 @@
 const input = document.querySelector('.main-input')
 const searchBtn = document.querySelector('.search-button')
+const bookList = document.querySelectorAll('.book')
+let bookClassList = []
 
-searchBtn.addEventListener('click',(event) => {
-    console.log(input.value)
-})
+for (let i = 0; i < bookList.length;i ++){
+    bookClassList.push(bookList[i].classList[1])
+}
+
+
+
+function Search() {
+    searchBtn.addEventListener('click',(event) => {
+        let inputValue = input.value
+        if (inputValue) {
+            console.log(inputValue)
+        }  
+    })
+    
+}
+
+Search()
